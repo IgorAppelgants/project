a33021d8d2759b951609b7c2aaf5107ff6d946cc
Igor <igor.appelgants@yandex.ru>
2021-10-12 22:35:04 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// const result = confirm('are you ready?');
+// console.log(result);
+
+// const answer = prompt('Сколько вам лет?', "");
+
+// console.log(answer);
+
+const answers = [];
+
+answers[0] = prompt('Как вас зовут?', '');
+answers[1] = prompt('Сколько вам лет?', '');
+answers[2] = prompt('Где вы живете?', '');
+
+document.write(answers);
